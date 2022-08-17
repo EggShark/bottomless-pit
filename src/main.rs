811600619ab791ba146d7fc25188d16f0174fd83
TheEggShark <ther3al3ggshark@gmail.com>
2022-08-17 11:40:04 -0500

    bevy test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Charles Crabtree".to_string()));
+    commands.spawn().insert(Person).insert(Name("Ann Crabtree".to_string()));
+    commands.spawn().insert(Person).insert(Name("Travis Crabtree".to_string()));
+}
+
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished(){
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+fn hello_world() {
+    println!("h")
 }
