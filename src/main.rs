06f244e8af33638305369d676d19383b47e87c59
TheEggShark <ther3al3ggshark@gmail.com>
2022-08-18 16:22:20 -0500

    simple momvment

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,25 @@ fn main() {
         .title("cheesed to meet u")
         .build();
     
-    rl.set_exit_key(Some(KeyboardKey::KEY_O));
+    rl.set_exit_key(None);
+
+    let mut circle_x: i32 = 0;
+    let mut circle_y: i32 = 0;
 
     while !rl.window_should_close() {
         let mut drawer = rl.begin_drawing(&thread);
-
+        if drawer.is_key_pressed(KeyboardKey::KEY_RIGHT) {
+            circle_x += 5;
+        } else if drawer.is_key_pressed(KeyboardKey::KEY_LEFT) {
+            circle_x -= 5;
+        } else if drawer.is_key_pressed(KeyboardKey::KEY_DOWN) {
+            circle_y += 5;
+        } else if drawer.is_key_pressed(KeyboardKey::KEY_UP) {
+            circle_y -= 5;
+        }
+    
+        drawer.draw_rectangle(circle_x, circle_y, 10, 10, Color::WHITE);
         drawer.clear_background(Color::BLUE);
-        drawer.draw_text("Cheesed to meet u", 12, 12, 12, Color::GOLD);
+        drawer.draw_text("Cheesed to meet u", 12, 12, 1, Color::GOLD);
     }
 }
