f5a67621e7afaf3d4cd19fee9be330dfb65ea7ca
TheEggShark <theeggshark@gmail.com>
2022-09-21 11:19:47 -0500

    added into for point

--- src/utils/point.rs
+++ src/utils/point.rs
@@ -1,5 +1,13 @@
+use raylib::core::math::Vector2;
+
 #[derive(Debug, PartialEq)]
 pub struct Point {
     pub x: i32,
     pub y: i32,
+}
+
+impl Into<Vector2> for Point {
+    fn into(self) -> Vector2 {
+        Vector2::new(self.x as f32, self.y as f32)
+    }
 }
